323a0b12d39f6b686ee87b13ed3d090dd66f5e76
Iskra B <ibatistapoblete@gmail.com>
2019-04-24 05:29:18 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,5 @@
 # Build your say_hello method here
 def say_hello(name = "Ruby Programmer")
-  puts "Hello #{name}"
+  puts "Hello, #{name}"
 end
 
-accepts an argument of a name and prints out Hello with that Name (FAILED - 1)
-  defaults to Ruby Programmer when no name is passed in
